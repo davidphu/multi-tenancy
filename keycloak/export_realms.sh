e964baaa567fbbf1dff86aca39742c8650bad935
David Phu <davidphu2014@gmail.com>
2021-02-28 09:33:05 -0500

    new keycloak docker-compose yml

---
+++ keycloak/export_realms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker exec -it kc /opt/jboss/keycloak/bin/standalone.sh \
+-Djboss.socket.binding.port-offset=100 \
+-Dkeycloak.migration.action=export \
+-Dkeycloak.migration.provider=dir \
+-Dkeycloak.migration.dir=/tmp/realms \
+-Dkeycloak.migration.usersPerFile=1000 \
+-Dkeycloak.migration.strategy=OVERWRITE_EXISTING
